38294edcf276aff7298ee0d158077719273c4e84
patrick <1423602+pdm-pcb@users.noreply.github.com>
2023-11-18 15:36:26 -0600

    Shuffling things about

--- cmake/FetchDeps.cmake
+++ cmake/FetchDeps.cmake
@@ -1,11 +1,16 @@
-include(FetchContent)
+function(FetchDeps)
+    include(FetchContent)
 
-FetchContent_Declare(
-    spdlog EXCLUDE_FROM_ALL SYSTEM
-    GIT_REPOSITORY https://github.com/gabime/spdlog
-    GIT_TAG 7e635fca68d014934b4af8a1cf874f63989352b7 # 2023-07-08
-    FIND_PACKAGE_ARGS 1.12.0
-)
-set(SPDLOG_USE_STD_FORMAT "ON" CACHE BOOL "" FORCE)
+    message(STATUS "Fetching spdlog...")
+    FetchContent_Declare(
+        spdlog EXCLUDE_FROM_ALL SYSTEM
+        GIT_REPOSITORY https://github.com/gabime/spdlog
+        GIT_TAG 7e635fca68d014934b4af8a1cf874f63989352b7 # 2023-07-08
+        FIND_PACKAGE_ARGS 1.12.0
+    )
 
-FetchContent_MakeAvailable(spdlog)
+    # Have spdlog to use std::format since C++20 support is better nowadays
+    set(SPDLOG_USE_STD_FORMAT "ON" CACHE BOOL "" FORCE)
+
+    FetchContent_MakeAvailable(spdlog)
+endfunction()
